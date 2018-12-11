f7d34c0b7b70699dfe3ff14ad6be645365e81cb6
BT_1N3_05 <jhoang6606@bths.edu>
2018-12-11 10:00:24 -0500

    a

--- src/Deck.java
+++ src/Deck.java
@@ -2,21 +2,33 @@ import java.util.ArrayList;
 
 public class Deck
 {
-    ArrayList<Card>unDealt;
-    ArrayList<Card>Dealt;
+    private ArrayList<Card>unDealt;
+    private ArrayList<Card>Dealt;
 
     public Deck(String[] ranks, String[] suits, int[] pointValues)
     {
+        unDealt = new ArrayList<>();
+        Dealt = new ArrayList<>();
         for(int i=0;i<ranks.length;i++)
         {
             for(int j=0;j<suits.length;j++)
             {
                 for(int k=0;k<pointValues.length;k++)
                 {
-
+                    unDealt.add(new Card(ranks[i],suits[j],pointValues[k]));
                 }
             }
         }
+    }
+
+    public ArrayList<Card> getUnDealt()
+    {
+        return unDealt;
+    }
+
+    public ArrayList<Card> getDealt()
+    {
+        return Dealt;
     }
 
     public boolean isEmpty()
@@ -33,8 +45,32 @@ public class Deck
         return unDealt.size();
     }
 
+    public Card deal()
+    {
+        if(unDealt.size()>0)
+        {
+            Card temp = unDealt.get(0);
+            Dealt.add(temp);
+            unDealt.remove(0);
+            return temp;
+        }
+        return null;
+    }
+
     public void shuffle()
     {
+        unDealt.addAll(Dealt);
+        while(Dealt.size()>0)
+        {
+            Dealt.remove(0);
+        }
+        for(int i=unDealt.size()-1;i>0;i--)
+        {
+            int pos = (int) (Math.random() * i);
+            Card temp = unDealt.get(i);
+            unDealt.set(i,unDealt.get(i));
+            unDealt.set(pos,temp);
 
+        }
     }
 }
